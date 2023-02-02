24b6a1b1739d0fe8882d3c1b428eebc575957b85
corno <corno.astn.a@schraverus.com>
2023-02-02 10:49:53 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,8 +7,6 @@ const d = pr.wrapRawDictionary
 import { $ as api } from "./api.p"
 
 export const $: mproject.TProject = {
-    'name': "XXXX",
-
     'author': "Corno",
     'description': "resource for escaping ASTN strings",
     'license': "ISC",
