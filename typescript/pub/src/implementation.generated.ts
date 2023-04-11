3b2121eee2c67cda22e5351242297c04210e9185
corno <corno.astn.a@schraverus.com>
2023-04-11 15:37:51 +0200

    .

--- typescript/pub/src/implementation.generated.ts
+++ typescript/pub/src/implementation.generated.ts
@@ -2,7 +2,7 @@ import { API } from "./api.generated"
 import { $$ as iescapeMultilineString } from "./implementations/escapeMultilineString.native"
 import { $$ as iescapeString } from "./implementations/escapeString.native"
 
-export const $r: API = {
+export const $api: API = {
     'escapeMultilineString': iescapeMultilineString,
     'escapeString': iescapeString,
 }
